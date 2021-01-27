a9b2a08a84333c82547168e4e367b2772c328527
vicnx <andanivicente@gmail.com>
2021-01-28 00:08:09 +0100

    TEST COMMIT

--- .github/actions/send_telegram_msg/index.js
+++ .github/actions/send_telegram_msg/index.js
@@ -1,13 +1,15 @@
 const core = require("@actions/core");
+const github = require("@actions/github") 
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = core.getInput("token_bot");
 const bot = new TelegramBot(token, {polling: false});
 var chatId=core.getInput("id_chat");
 const name = core.getInput("name");
+const msg = github.event.head_commit.message
 
 try {
-  bot.sendMessage(chatId,"Workflow ejecutado correctamente tras el último commit. Saludos "+name);
+  bot.sendMessage(chatId,msg);
 } catch (error) {
   core.setFailed(error.message);
 }
